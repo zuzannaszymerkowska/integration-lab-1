42beef4d2dfa0644ec35fdd4df7d15da0560704b
Zuzanna Szymerkowska <zuza.szymerkowska12@gmail.com>
2026-03-05 09:00:18 +0100

    Implement basic views and templates for blog posts

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import PostListView, PostDetailView
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html' # ścieżka do szablonu
+    context_object_name = 'posts'         # nazwa zmiennej w HTML
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
