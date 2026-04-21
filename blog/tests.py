9cfe3a4379e13d3faf9188b6e4c99d3a73171942
Zuzanna Szymerkowska <zuza.szymerkowska12@gmail.com>
2026-04-21 16:39:43 +0200

    Introduce intentional test failure

--- blog/tests.py
+++ blog/tests.py
@@ -7,8 +7,8 @@ from django.urls import reverse
 class BlogTests(TestCase):
     def test_homepage_status_code(self):
         response = self.client.get(reverse('post_list'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
 
     def test_api_status_code(self):
         response = self.client.get(reverse('api_posts_json'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
